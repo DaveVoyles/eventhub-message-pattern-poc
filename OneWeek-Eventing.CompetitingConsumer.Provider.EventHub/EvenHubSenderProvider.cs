57789265aaf91384bf40b0301bb6c3522ed19f9f
Dave Voyles <dvoyles@microsoft.com>
2019-11-05 16:49:23 -0800

    Added empty event hub sender provider

---
+++ OneWeek-Eventing.CompetitingConsumer.Provider.EventHub/EvenHubSenderProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Azure.EventHubs;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneWeek_Eventing.CompetitingConsumer.Provider.EventHub
+{
+    class EvenHubSenderProvider
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
