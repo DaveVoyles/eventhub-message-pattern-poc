ecb790d54a21707dfe66d41f9e768bfbd99c2c88
Dave Voyles <dvoyles@microsoft.com>
2019-11-05 17:30:23 -0800

    Began adding stubbed out interface functions to sender provider

--- OneWeek-Eventing.CompetitingConsumer.Provider.EventHub/EvenHubSenderProvider.cs
+++ OneWeek-Eventing.CompetitingConsumer.Provider.EventHub/EvenHubSenderProvider.cs
@@ -2,14 +2,70 @@
 using Microsoft.Azure.EventHubs;
 using System.Text;
 using System.Threading.Tasks;
+using OneWeek_Eventing.CompetingConsumer.Entities;
+using OneWeek_Eventing.CompetingConsumer.Interfaces;
 
-namespace OneWeek_Eventing.CompetitingConsumer.Provider.EventHub
+
+
+namespace OneWeek_Eventing.CompetingConsumer.Provider.EventHub
 {
-    class EvenHubSenderProvider
+    class EvenHubSenderProvider : ISenderProvider
     {
-        static void Main(string[] args)
+        private static EventHubClient eventHubClient;
+        private const string EventHubConnectionString = "Endpoint=sb://dv-eventhub.servicebus.windows.net/;SharedAccessKeyName=manage-policy;SharedAccessKey=2Ryi+hFlavlPN3aEir0YsEEBhuCgtD7+kXnvvIs1VEw=;EntityPath=myeventhub";
+        private const string EventHubName             = "myeventhub";
+
+        //private static async Task MainAsync(string[] args)
+        public Task Start(string instrument = null, int partitionCount = -1)
         {
-            Console.WriteLine("Hello World!");
+            // Creates an EventHubsConnectionStringBuilder object from the connection string, and sets the EntityPath.
+            // Typically, the connection string should have the entity path in it, but this simple scenario
+            // uses the connection string from the namespace.
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EventHubConnectionString)
+            {
+                EntityPath = EventHubName
+            };
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            //await SendMessagesToEventHub(100);
+            //await eventHubClient.CloseAsync();
+
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+
+            return Task.CompletedTask;
         }
+
+        public Task Stop()
+        {
+            // TODO: Probably need to replace this line
+            return Task.CompletedTask;
+        }
+
+        public async Task SendMessageAsync(Trade trade) { 
+        }
+
+        // Uses the event hub client to send 100 messages to the event hub.
+        private static async Task SendMessagesToEventHub(int numMessagesToSend)
+        {
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+                    var message = $"Message {i}";
+                    Console.WriteLine($"Sending message: {message}");
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+                await Task.Delay(10);
+            }
+            Console.WriteLine($"{numMessagesToSend} messages sent.");
+        }
+
+
     }
 }
