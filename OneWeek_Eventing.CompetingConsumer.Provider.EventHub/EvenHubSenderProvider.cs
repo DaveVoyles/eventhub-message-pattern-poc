0630f72a1ce79c0c115f7a9804d0cf76f981857e
Dave Voyles <dvoyles@microsoft.com>
2019-11-06 10:26:05 -0800

    Saving before creating new fork for DV

--- OneWeek_Eventing.CompetingConsumer.Provider.EventHub/EvenHubSenderProvider.cs
+++ OneWeek_Eventing.CompetingConsumer.Provider.EventHub/EvenHubSenderProvider.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Microsoft.Azure.EventHubs;
 using OneWeek_Eventing.CompetingConsumer.Entities;
 using OneWeek_Eventing.CompetingConsumer.Interfaces;
+using System.Text;
 
 namespace OneWeek_Eventing.CompetingConsumer.Provider.EventHub
 {
@@ -10,10 +12,16 @@ namespace OneWeek_Eventing.CompetingConsumer.Provider.EventHub
     {
         private static EventHubClient eventHubClient;
         private const string EventHubConnectionString = "Endpoint=sb://dv-eventhub.servicebus.windows.net/;SharedAccessKeyName=manage-policy;SharedAccessKey=2Ryi+hFlavlPN3aEir0YsEEBhuCgtD7+kXnvvIs1VEw=;EntityPath=myeventhub";
-        private const string EventHubName = "myeventhub";
+        private const string EventHubName             = "myeventhub";
+
+        private string _instrument;
+        private int    _partitionCount;
 
         public Task Start(string instrument = null, int partitionCount = -1)
         {
+            _instrument     = instrument;
+            _partitionCount = partitionCount;
+
             // Creates an EventHubsConnectionStringBuilder object from the connection string, and sets the EntityPath.
             // Typically, the connection string should have the entity path in it, but this simple scenario
             // uses the connection string from the namespace.
@@ -21,48 +29,39 @@ namespace OneWeek_Eventing.CompetingConsumer.Provider.EventHub
             {
                 EntityPath = EventHubName
             };
-
             eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
-
-            //await SendMessagesToEventHub(100);
-            //await eventHubClient.CloseAsync();
-
-            Console.WriteLine("Press ENTER to exit.");
-            Console.ReadLine();
 
             return Task.CompletedTask;
         }
 
         public async Task SendMessageAsync(Trade trade)
         {
+            //if (String.IsNullOrEmpty(_instrument) || String.Compare(_instrument, trade.Instrument, false) == 0)
+            //{
+            //    var subscriber = _redis.GetSubscriber();
+            //    var tradeAsJson = JsonConvert.SerializeObject(trade);
+
+            //    await subscriber.PublishAsync($"Trades-{trade.Instrument}", tradeAsJson);
+            //    if (String.IsNullOrEmpty(_instrument))
+            //        await subscriber.PublishAsync("Trades-*", tradeAsJson);
+            //    else if (_partitionCount != -1)
+            //        await subscriber.PublishAsync($"Trades-{trade.GetPartitionIndex(_partitionCount)}", tradeAsJson);
+            //}
+
+
+            if (String.IsNullOrEmpty(_instrument) || String.Compare(_instrument, trade.Instrument, false) == 0)
+            {
+                var tradeAsJson    = JsonConvert.SerializeObject(trade);
+
+                await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(tradeAsJson))); // TODO: put trade here
+            }
         }
+
         public Task Stop()
         {
             // TODO: Probably need to replace this line
             return Task.CompletedTask;
         }
-
-        // Uses the event hub client to send 100 messages to the event hub.
-        //private static async Task SendMessagesToEventHub(int numMessagesToSend)
-        //{
-        //    for (var i = 0; i < numMessagesToSend; i++)
-        //    {
-        //        try
-        //        {
-        //            var message = $"Message {i}";
-        //            Console.WriteLine($"Sending message: {message}");
-        //            await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
-        //        }
-        //        catch (Exception exception)
-        //        {
-        //            Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
-        //        }
-        //        await Task.Delay(10);
-        //    }
-        //    Console.WriteLine($"{numMessagesToSend} messages sent.");
-        //}
-
-
     }
 }
 
