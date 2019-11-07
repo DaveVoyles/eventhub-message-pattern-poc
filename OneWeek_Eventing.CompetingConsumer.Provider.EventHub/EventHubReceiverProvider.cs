6b61fe4bf4bb4d396a716481d16d2fc42107fac4
Dave Voyles <dvoyles@microsoft.com>
2019-11-07 11:01:58 -0800

    Removed unnecessary code/comments. Ordering complete.

--- OneWeek_Eventing.CompetingConsumer.Provider.EventHub/EventHubReceiverProvider.cs
+++ OneWeek_Eventing.CompetingConsumer.Provider.EventHub/EventHubReceiverProvider.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.EventHubs.Processor;
 using OneWeek_Eventing.CompetingConsumer.Entities;
 using OneWeek_Eventing.CompetingConsumer.Interfaces;
-
 
 namespace OneWeek_Eventing.CompetingConsumer.Provider.EventHub
 {
@@ -16,7 +12,6 @@ namespace OneWeek_Eventing.CompetingConsumer.Provider.EventHub
         // Event Hub connections
         private const string EventHubConnectionString = "Endpoint=sb://dv-eventhub.servicebus.windows.net/;SharedAccessKeyName=manage-policy;SharedAccessKey=2Ryi+hFlavlPN3aEir0YsEEBhuCgtD7+kXnvvIs1VEw=;EntityPath=myeventhub";
         private const string EventHubName             = "myeventhub";
-        private static EventHubClient eventHubClient;
 
         // Storage
         private const string StorageContainerName              = "hubcontainer";
@@ -24,8 +19,8 @@ namespace OneWeek_Eventing.CompetingConsumer.Provider.EventHub
         private const string StorageAccountKey                 = "6XY4vaNFeafilm1n5rU4toc1a6Y4IlqfMRoWuMz1ESpTQjM8a2A5El2NEjKXNPEE87qUbs+y69V9A4uM+/YR9g==";
         private static readonly string StorageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", StorageAccountName, StorageAccountKey);
 
-        private string _instrument;
-    
+        private EventProcessorHost eventProcessorHost;
+
         // Get context for EventHubReceiverProvider, to be used in/by EventHubProcessor
         public static EventHubReceiverProvider current;
         public EventHubReceiverProvider()
@@ -35,33 +30,29 @@ namespace OneWeek_Eventing.CompetingConsumer.Provider.EventHub
 
         public async Task Start(string instrument, bool usePartitions, int partitionIndex = -1, int partitionCount = -1)
         {
-            _instrument = instrument;
-
             // Connect to event processor using Event Hub & Storage credentials
-            var eventProcessorHost = new EventProcessorHost(
-                EventHubName,
+            eventProcessorHost = new EventProcessorHost(
+                EventHubName                              ,
                 PartitionReceiver.DefaultConsumerGroupName,
-                EventHubConnectionString,
-                StorageConnectionString,
-                StorageContainerName);
+                EventHubConnectionString                  ,
+                StorageConnectionString                   , 
+                StorageContainerName                     );
 
             // Registers the Event Processor Host and starts receiving messages
             await eventProcessorHost.RegisterEventProcessorAsync<EventHubProcessor>();
-
-            // Disposes of the Event Processor Host
-            //await eventProcessorHost.UnregisterEventProcessorAsync();
         }
 
-        // Trade events
+        // Trade events, invoked when a trade is received
         public event EventHandler<Trade> OnTradeReceived;
         public void SendTradeEvent(Trade t)
         {
             OnTradeReceived?.Invoke(this, t);
         }
 
-        public Task Stop()
+        public async Task Stop()
         {
-            throw new NotImplementedException();
+            // Disposes of the Event Processor Host
+            await eventProcessorHost.UnregisterEventProcessorAsync();
         }
     }
 }
